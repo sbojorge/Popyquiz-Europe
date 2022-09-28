270e3b0a38acbdbbc446e059b7b1f47f30fb62b5
sbojorge <sara.bojorge@gmail.com>
2022-09-28 15:46:39 +0000

    start adding questions on the script.js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+document.getElementById('start-btn').addEventListener('click');
+
+
+let myQuestions = [
+    { 
+        question: 'Cristiano Ronaldo, the famous football player, was born in:',
+        answers: [
+            {text: 'Luxembourg', value: false},
+            {text: 'Portugal', value: true},
+            {text: 'Spain', value: false}
+        ]
+    },
+    {
+        question: 'The capital of Switzerland is:',
+        answers: [
+            {text: 'Bern', value: true},
+            {text: 'Zurich', value: false},
+            {text: 'Grass', value: false}
+        ]
+    }
+]
