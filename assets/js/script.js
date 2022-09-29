afcfb5d2e3af8b540cdcfa3409249eca9995e746
sbojorge <sara.bojorge@gmail.com>
2022-09-29 20:34:35 +0000

    update home page style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,5 @@
 
-document.getElementById('start-btn').addEventListener('click');
+let start = document.getElementById('start-btn';
+start.addEventListener('click');
 
 
-let myQuestions = [
-    { 
-        question: 'Cristiano Ronaldo, the famous football player, was born in:',
-        answers: [
-            {text: 'Luxembourg', value: false},
-            {text: 'Portugal', value: true},
-            {text: 'Spain', value: false}
-        ]
-    },
-    {
-        question: 'The capital of Switzerland is:',
-        answers: [
-            {text: 'Bern', value: true},
-            {text: 'Zurich', value: false},
-            {text: 'Grass', value: false}
-        ]
-    }
-]
