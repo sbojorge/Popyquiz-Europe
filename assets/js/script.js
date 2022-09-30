a92d96e9015ca41f97c16f47d1b439a51d0fa7d5
sbojorge <sara.bojorge@gmail.com>
2022-09-30 19:19:15 +0000

    fix footer to the bottom of the viewport

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,7 @@
 let start = document.getElementById('start-btn');
 start.addEventListener('click', function(){ alert("Hello World!") });
 
+let myQuiz = {question:'what is the capital of Spain?', answers : ['Lisbone', 'Paris', 'Madrid'], correctAns: 'Madrid'};
+console.log(myQuiz);
+
 
