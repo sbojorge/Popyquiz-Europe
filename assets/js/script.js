156a0fc04f06e8aa8a07fd7f5bcc31f2eeda02d9
sbojorge <sara.bojorge@gmail.com>
2022-09-30 11:05:42 +0000

    start setting structure for quiz area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
 let start = document.getElementById('start-btn');
-start.addEventListener('click', function(){ alert("Hello World!"); });
+start.addEventListener('click', function(){ alert("Hello World!") });
 
 
