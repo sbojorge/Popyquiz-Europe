984ad556bb7a35a9a52bdb93f150f7856171ea1b
sbojorge <sara.bojorge@gmail.com>
2022-10-04 08:57:54 +0000

    work on start quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,50 @@ document.addEventListener("DOMContentLoaded", function() {
     
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "next") {
+            if (this.getAttribute("data-type") === "start") {
+              startQuiz();
+            } else if (this.getAttribute("data-type") === "next") {
                 alert("You clicked next!");
             } else {
-                let answers = this.getAttribute("data-type");
+                let answers = this.getAttribute("data-type") ;
                 alert(`You clicked ${answers}`);
             }
         });
     }
+
+    startQuiz(question);
 });
 
-function startQuiz() {
+
+    
+
+
+
+
+/** Pick a question randomly and its possible answers on the userinterface */
+
+function startQuiz(question) {
+  
+  let myQuiz = [{    
+    q :'What is the capital of Spain?', 
+    a : ['Lisbon', 'Paris', 'Madrid'],
+    correctAnswer: 'Madrid'
+},
+{
+  q : 'Red Devils is the national football team of:',
+  a: ['France', 'Belgium', 'Finland'],
+  correctAnswer: 'Belgium'
+}];
+
+let question = myQuiz[Math.floor(Math.random()*myQuiz.length)];
 
 }
 
 function displayQuestion() {
-
+  document.getElementById('question').innerHTML = question.q;
+  document.getElementById('opt1').innerHTML = question.a[0];
+  document.getElementById('opt2').innerHTML = question.a[1];
+  document.getElementById('opt3').innerHTML = question.a[2];
 }
 
 function checkAnswer() {
