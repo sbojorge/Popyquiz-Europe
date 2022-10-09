902a95e83f2872f57480b4302bf97675beee16c5
sbojorge <sara.bojorge@gmail.com>
2022-10-09 18:53:25 +0000

    fix checkanswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,134 +1,136 @@
-
 //These are the questions for the quiz
 
-var myQuiz = [{    
+let myQuiz = [{    
   q :'Spain has __ UNESCO World Heritage Site: ', 
   a : [48, 10, 14],
-  correctAnswer: '48'
+  correctAnswer : '48'
 },
 {
   q : 'Dutch, French and German are the official languages of:',
-  a: ['Luxembourg', 'Belgium', 'Switzerland'],
-  correctAnswer: 'Belgium'
+  a : ['Luxembourg', 'Belgium', 'Switzerland'],
+  correctAnswer : 'Belgium'
 },
 {
-  q: 'Which of these cities is the birthplace of the famous composer Mozart?:',
-  a: ['Linz', 'Salzburg', 'Vienna'],
-  correctAnswer: 'Salzburg'
+  q : 'Which of these cities is the birthplace of the famous composer Mozart?:',
+  a : ['Linz', 'Salzburg', 'Vienna'],
+  correctAnswer : 'Salzburg'
 },
 {
   q : 'Ljubljana is the capital of:',
-  a: ['Slovenia', 'Slovakia', 'Poland'],
-  correctAnswer: 'Slovenia'
+  a : ['Slovenia', 'Slovakia', 'Poland'],
+  correctAnswer : 'Slovenia'
 },
 {
-  q: "World's most active volcanoes are located in this country:",
-  a: ['Norway', 'Italy', 'Austria'],
-  correctAnswer: 'Italy'
+  q : "World's most active volcanoes are located in this country:",
+  a : ['Norway', 'Italy', 'Austria'],
+  correctAnswer : 'Italy'
 },
 {
-  q: 'Cristiano Ronaldo, the famous football player, was born in:',
-  a: ['Portugal', 'Spain', 'France'],
-  correctAnswer: 'Portugal'
+  q : 'Cristiano Ronaldo, the famous football player, was born in:',
+  a : ['Portugal', 'Spain', 'France'],
+  correctAnswer : 'Portugal'
 },
 {
-  q: 'The hottest country in Europe is:',
-  a: ['Malta', 'Italy', 'Spain'],
-  correctAnswer: 'Malta'
+  q : 'The hottest country in Europe is:',
+  a : ['Malta', 'Italy', 'Spain'],
+  correctAnswer : 'Malta'
 },
 {
-  q: 'The capital of Switzerland is:',
-  a: ['Zurich', 'Bern', 'Grass'],
-  correctAnswer: 'Bern'
+  q : 'The capital of Switzerland is:',
+  a : ['Zurich', 'Bern', 'Grass'],
+  correctAnswer : 'Bern'
 },
 {
-  q: 'The croissant is originally from:',
-  a: ['Paris', 'Vienna', 'Brussels'],
-  correctAnswer: 'Vienna'
+  q : 'The croissant is originally from:',
+  a : ['Paris', 'Vienna', 'Brussels'],
+  correctAnswer : 'Vienna'
 },
 {
-  q: 'Olympic Games have their origin in:',
-  a: ['Greece', 'Croatia', 'Italy'],
-  correctAnswer: 'Greece'
+  q : 'Olympic Games have their origin in:',
+  a : ['Greece', 'Croatia', 'Italy'],
+  correctAnswer : 'Greece'
 },
 {
-  q: '___ is the 5th smallest country in the European Union',
-  a: ['Andorra', 'Liechtenstein', 'Slovenia'],
-  correctAnswer: 'Slovenia'
+  q : '___ is the 5th smallest country in the European Union',
+  a : ['Andorra', 'Liechtenstein', 'Slovenia'],
+  correctAnswer : 'Slovenia'
 },
 {
-  q:'This country has a unique active geyser:',
-  a: ['Iceland', 'Slovenia','Slovakia'],
-  correctAnswer: 'Slovakia'
+  q :'This country has a unique active geyser:',
+  a : ['Iceland', 'Slovenia','Slovakia'],
+  correctAnswer : 'Slovakia'
 },
 {
-  q: 'More Than Half of the Olive Oil in the World Is Produced in:',
-  a: ['Spain', 'Greece', 'Italy'],
-  correctAnswer: 'Spain'
+  q : 'More Than Half of the Olive Oil in the World Is Produced in:',
+  a : ['Spain', 'Greece', 'Italy'],
+  correctAnswer : 'Spain'
 },
 {
-  q: 'In which of these Italian cities was the pizza invented:',
-  a: ['Florence', 'Rome', 'Naples'],
-  correctAnswer: 'Naples'
+  q : 'In which of these Italian cities was the pizza invented:',
+  a : ['Florence', 'Rome', 'Naples'],
+  correctAnswer : 'Naples'
 },
 {
-  q: 'Italy has 2 independent countries within its area. These countries are:',
-  a: ['Meurthe-et-Moselle', 'San Marino and Vatican City', 'Venice and Tuscany'],
-  correctAnswer: 'San Marino and Vatican City'
+  q : 'Italy has 2 independent countries within its area. These countries are:',
+  a : ['Meurthe-et-Moselle', 'San Marino and Vatican City', 'Venice and Tuscany'],
+  correctAnswer : 'San Marino and Vatican City'
 }];
 
 // Wait for the DOM to be loaded
 
-document.addEventListener("DOMContentLoaded", displayQuestion);
-
-let firstButton = document.getElementById("start-btn");
-firstButton.addEventListener("click", function() {
-  document.getElementById("homepage-container").style.display = "none";
-  document.getElementById("general-container").style.display = "block";
+document.addEventListener("DOMContentLoaded", function() {
+  let startQuiz = document.getElementById('start-btn');
+  startQuiz.addEventListener('click', function(){
+    document.getElementById('homepage-container').style.display = 'none';
+    document.getElementById('general-container').style.display = 'block';
+  })
 })
+  
+/** Displays a question and its multiple choice answer right after the user clicks on
+ * the Let's go!, Next or Take the quiz again buttons respectively
+ */
 
-//Randomly pick a question and its possible answers and display them on the user interface
-
-  function displayQuestion(event) {
-
-    let question = myQuiz[Math.floor(Math.random() * myQuiz.length)];
-    document.getElementById('question').innerHTML = question.q;
-    document.getElementById('opt1').innerHTML = question.a[0];
-    document.getElementById('opt2').innerHTML = question.a[1];
-    document.getElementById('opt3').innerHTML = question.a[2];
-  }
-
-// Check user's choice and if right then console log 'bravo' otherwise console log 'try again'
+ let quiz = myQuiz[Math.floor(Math.random() * myQuiz.length)];
+ document.getElementById('question').innerHTML = quiz.q;
+ document.getElementById('opt1').innerHTML = quiz.a[0];
+ document.getElementById('opt2').innerHTML = quiz.a[1];
+ document.getElementById('opt3').innerHTML = quiz.a[2];
+ 
+ 
+ 
+/** 
+ * Check user's choice and if right then console log 'bravo' otherwise console log 'try again'
+ */ 
 
 let options = document.getElementsByClassName("answers");
-
-function handleBtnClick(event) {
-  let correctAnswers = question.correctAnswer; 
+ 
+function handleOptClicked(event) {
+  let rightAnswers = quiz.correctAnswer;
   let selectedOption = this.innerHTML;
-  if (selectedOption === correctAnswers) {
+  if (selectedOption === rightAnswers) {
     alert('Bravo!');
   } else {
     alert('Try again')
   }
-} 
-  
+}
+
 for (let option of options) {
-    option.addEventListener("click", handleBtnClick);
+  option.addEventListener("click", handleOptClicked);
 }
 
-// Display a new question /
+/** Display a new question after 'Next' button has been clicked */  
 
-let newQuestion = document.getElementById("nxt-btn");
+let newQuestion = document.getElementById("nxt-btn");    
+newQuestion.addEventListener("click", function() {
 
-function myFunction() {
-  let question = myQuiz[Math.floor(Math.random()*myQuiz.length)];
-  document.getElementById('question').innerHTML = question.q;
-  document.getElementById('opt1').innerHTML = question.a[0];
-  document.getElementById('opt2').innerHTML = question.a[1];
-  document.getElementById('opt3').innerHTML = question.a[2];  
-}
+  let quiz = myQuiz[Math.floor(Math.random()*myQuiz.length)];
+  console.log(quiz);
+  document.getElementById('question').innerHTML = quiz.q;
+  document.getElementById('opt1').innerHTML = quiz.a[0];
+  document.getElementById('opt2').innerHTML = quiz.a[1];
+  document.getElementById('opt3').innerHTML = quiz.a[2];
 
-newQuestion.addEventListener("click", myFunction);
+})
 
 function displayModal() {
 
@@ -137,6 +139,20 @@ function displayModal() {
 function quizAgain() {
   
 }
+  
+
+
+    
+
+
+
+
+
+
+
+
+
+
 
 
 
