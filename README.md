97966c17527b391a5a9f99274cca8ce83dfb6b28
sbojorge <sara.bojorge@gmail.com>
2022-10-10 09:05:00 +0000

    add comments in script.js and keep working on README.md

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Popy Quiz Europe is an amusement, short quiz which purpose is to let the user to
 The project was inspired from our family trip last summer: While we enjoyed discovering new countries we strove to teach our 7 years-old about them. Sometimes we found ourselves learning too.
 Considering that, the content of the quiz and its design have been made to be accessible to general public from young children to grown-up of all ages.
 
-With the intention of not discouraging our young public, it's been decided to have none score and not to disable the answers once the user has made a choice. The user can use then all his/her chances until discovering the right answer.
+With the intention of not discouraging our young public, it's been decided to have none score and not to disable the answers once the user has made a choice. The user can use then all his/her chances until discovering the right answer and/or interact with the same question as long and as many times as desired.
 
 The website is responsive in the most common screen devices (smartphone, tablet, laptop and desktop) and within the following width categories:
 
@@ -14,8 +14,45 @@ The website is responsive in the most common screen devices (smartphone, tablet,
 * 1280 x 802px
 * 1600 x 992px
 
-![This is a mockup image of the deployed website](/assets/images/mockup.webp)
+![This is a mockup image of the deployed website](/assets/images/picture1.webp)
 
 ## Technologies
 
 This web application was built using HTML, CSS and JavaScript.
+
+## Features
+
+The website's home page was designed with a "mobile first" intention in mind: tell the users who we are, what we do/offer and a call to take action.
+
+The background image and colors were selected to subtly recall the EU colors and thus reinforce the topic in the user's mind.
+
+The home page includes:
+
+* The logo (name of the website)
+
+![This is the logo](/assets/images/picture2.webp)
+
+Popy quiz is a wordplay about "pop quiz"(surprise quiz) : 'Pop + y', adding a childish side that would gets kid's interest, + 'Quiz', a short and easy-going entertaning pastime.
+
+* Presentation of the quiz topic and content
+
+![This is the presentation](/assets/images/picture3.webp)
+
+The user is introduced to the topic of the questions. It would create an expectation.
+
+* An invitation to action : "Let's go!" button
+
+![This is the Let's go! button](/assets/images/picture4.webp)
+
+The tone has been set, user's attention is got. The user is invited to interact by clicking this button.
+Using Javascript the "Let's go!" button changes the home page into the user interface: the quiz area.
+
+* Footer
+
+![This is the footer of the website](/assets/images/picture5.webp)
+
+Holds an invitation to the user to spread this quiz through his/her social media.
+
+All links have an aria-label attribute for accesibility reasons and open in a new tab so the user can stays on Popy Quiz.
+
+The footer remains available at user's hand during the whole visit. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //These are the questions for the quiz
 
-let myQuiz = [{    
+let myQuiz = [{
   q :'Spain has __ UNESCO World Heritage Site: ', 
   a : [48, 10, 14],
   correctAnswer : '48'
@@ -73,77 +73,82 @@ let myQuiz = [{
 {
   q : 'Italy has 2 independent countries within its area. These countries are:',
   a : ['Meurthe-et-Moselle', 'San Marino and Vatican City', 'Venice and Tuscany'],
-  correctAnswer : 'San Marino and Vatican City'
-}];
+  correctAnswer : 'San Marino and Vatican City' 
+  
+}]
 
 // Wait for the DOM to be loaded
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
   let startQuiz = document.getElementById('start-btn');
   startQuiz.addEventListener('click', function(){
     document.getElementById('homepage-container').style.display = 'none';
     document.getElementById('general-container').style.display = 'block';
+    getNewQuestion();
   })
 })
-  
-/** Displays a question and its multiple choice answer right after the user clicks on
- * the Let's go!, Next or Take the quiz again buttons respectively
+
+let currentQuestion;
+
+/** Randomly picks a question and its possible answers,
+ *  displays them on the UI, verifies the answer clicked by the user
+ *  and displays the result
  */
+ function getNewQuestion() {
+  //Select random item from an array comes from https://css-tricks.com/snippets/javascript/select-random-item-array/
+  currentQuestion = myQuiz[Math.floor(Math.random() * myQuiz.length)]; 
+  document.getElementById('question').innerHTML = currentQuestion.q;
+  document.getElementById('opt1').innerHTML = currentQuestion.a[0];
+  document.getElementById('opt2').innerHTML = currentQuestion.a[1];
+  document.getElementById('opt3').innerHTML = currentQuestion.a[2];
+ }
 
- let quiz = myQuiz[Math.floor(Math.random() * myQuiz.length)];
- document.getElementById('question').innerHTML = quiz.q;
- document.getElementById('opt1').innerHTML = quiz.a[0];
- document.getElementById('opt2').innerHTML = quiz.a[1];
- document.getElementById('opt3').innerHTML = quiz.a[2];
- 
- 
- 
-/** 
- * Check user's choice and if right then console log 'bravo' otherwise console log 'try again'
- */ 
+// Check user's choice and if right then console log 'bravo' otherwise console log 'try again'
 
-let options = document.getElementsByClassName("answers");
- 
-function handleOptClicked(event) {
-  let rightAnswers = quiz.correctAnswer;
-  let selectedOption = this.innerHTML;
-  if (selectedOption === rightAnswers) {
-    alert('Bravo!');
-  } else {
-    alert('Try again')
+  let options = document.getElementsByClassName("answers");
+  
+  function handleOptClicked(event) {
+    let rightAnswers = currentQuestion.correctAnswer;
+    let selectedOption = this.innerHTML;
+    console.log(rightAnswers);
+    console.log(selectedOption);
+    if (selectedOption === rightAnswers) {
+      alert('Bravo!');
+    } else {
+      alert('Try again')
+    }
   }
-}
-
-for (let option of options) {
-  option.addEventListener("click", handleOptClicked);
-}
-
-/** Display a new question after 'Next' button has been clicked */  
+  
+  for (let option of options) {
+    option.addEventListener("click", handleOptClicked);
+  }
+ 
+// Display a new question after 'Next' button has been clicked 
 
 let newQuestion = document.getElementById("nxt-btn");    
-newQuestion.addEventListener("click", function() {
+newQuestion.addEventListener("click", getNewQuestion); 
 
-  let quiz = myQuiz[Math.floor(Math.random()*myQuiz.length)];
-  console.log(quiz);
-  document.getElementById('question').innerHTML = quiz.q;
-  document.getElementById('opt1').innerHTML = quiz.a[0];
-  document.getElementById('opt2').innerHTML = quiz.a[1];
-  document.getElementById('opt3').innerHTML = quiz.a[2];
 
-})
 
-function displayModal() {
+ 
+ 
+ 
 
-}
 
-function quizAgain() {
-  
-}
+
+
+ 
+
+
+
+
+
+
+
   
 
 
     
-
 
 
 
